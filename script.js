626aa31b1aa0c4d373862b968d465efe03cfabd6
saarahah <sarahlferguson07@gmail.com>
2020-06-20 01:12:08 -0400

    display time

--- script.js
+++ script.js
@@ -1,36 +1,11 @@
 var container = $(".container");
 var times = ["9", "10", "11", "12", "1", "2", "3", "4", "5"];
-// var words = localStorage.getItem("fields");
  var storedFields = localStorage.getItem("storedResponses");
-// var storedFields=[];
 console.log("this is stored fields: " + storedFields);
 
-
-var tk = times.keys();
-
-for (x of tk) {
-
-  $("#individualForms" + x).text(function() {
-    console.log("ran that boi");
-    console.log("x is " + x);
-    return storedFields;
-    // console.log("ran that boi");
-    // console.log(x);
-  });
-}
-
-
-
-// if (storedFields) {
-
-//storedFields = JSON.parse(storedFields);
-
-// } else {
-
-//   storedFields = {}   // hour: info
-
-// }
-
+setInterval(() => {
+  $("p#currentDay").text(moment().format("dddd, MMM Do YYYY"));
+}, 1000);
 
 
 for (var i = 0; i < times.length; i=i+1) {
@@ -51,9 +26,10 @@ var $hourInput = $('.container').append('<div class="time-block">' +
     console.log(savebutton);
 
 $('#hour' + i).append('<p>'+times[i]+'</p>');
- 
-$('#savebutton' + i).append("SAVE");
   }
+
+$('.saveBtn').append("SAVE");
+
 for(i=0; i<times.length; i++){
   if(localStorage.getItem(i)!=null){
 $('#individualForms'+i).text(localStorage.getItem(i))
@@ -63,12 +39,9 @@ $('#individualForms'+i).text(localStorage.getItem(i))
 ////////////////////////////////////////////////////////////////////
   $(document).on("click", ".saveBtn", function(e) {  
     alert("hello");
-
-   //var buttonID= $(this).attr('id') + "fields";
-
     var input = $(this).siblings("textarea").val();
 
-  //var input = JSON.stringify($(this).siblings("textarea").val());
+  
 
    var hour = $(this).attr("id")
    localStorage.setItem(hour, input);
@@ -76,23 +49,10 @@ $('#individualForms'+i).text(localStorage.getItem(i))
 
 
 
-  // storedFields[hour] = input;
-
-  //  console.log(tasks)
-
-  // localStorage.setItem("storedResponses", JSON.stringify(storedFields));
     
 
  localStorage.setItem("storedResponses", JSON.stringify($(this).siblings("textarea").val()));
 
-
-//console.log(JSON.parse(localStorage.getItem("savebutton0fields")));
-// $('fields').textContent = words;
-
-   //console.log($(this).siblings("textarea").val());
-   //console.log("local store item 0 is: " + localStorage.getItem("savebutton0fields"));
-//var storedFields = JSON.parse(localStorage.getItem("fields"));
-//$(this).Text(storedFields);
    
 });
 
