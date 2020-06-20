78cb7e64090a52a6343e63ae3ca8a74d417d3dc3
saarahah <sarahlferguson07@gmail.com>
2020-06-20 02:06:07 -0400

    colors finished

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var container = $(".container");
-var times = ["9", "10", "11", "12", "1", "2", "3", "4", "5"];
+var times = [9, 10, 11, 12, 1, 2, 3, 4, 5];
  var storedFields = localStorage.getItem("storedResponses");
-console.log("this is stored fields: " + storedFields);
+//console.log("this is stored fields: " + storedFields);
 
 setInterval(() => {
   $("p#currentDay").text(moment().format("dddd, MMM Do YYYY"));
@@ -13,9 +13,7 @@ var hourName = "hour" + i;
 var textInput= "form" +i;
 var savebutton=  i;
 var formsDiv = "individualForms" +i;
-console.log(i);
-
-
+//console.log(i);
 
 var $hourInput = $('.container').append('<div class="time-block">' +
     '<div class="row">' +
@@ -23,7 +21,7 @@ var $hourInput = $('.container').append('<div class="time-block">' +
     '<div class="col-sm saveBtn" id='+savebutton+'></div>'
   );
 
-    console.log(savebutton);
+    //console.log(savebutton);
 
 $('#hour' + i).append('<p>'+times[i]+'</p>');
   }
@@ -45,21 +43,45 @@ $('#individualForms'+i).text(localStorage.getItem(i))
 
    var hour = $(this).attr("id")
    localStorage.setItem(hour, input);
-   console.log("trying this shit: " + localStorage.getItem(hour));
-
-
-
-    
-
- localStorage.setItem("storedResponses", JSON.stringify($(this).siblings("textarea").val()));
-
-   
+   //.log("trying this shit: " + localStorage.getItem(hour));
+  localStorage.setItem("storedResponses", JSON.stringify($(this).siblings("textarea").val()));
 });
 
+function colorHours(){
+  var actualHour = moment().format("HH");
+  var index = 0;
+ // console.log(actualHour + "is time in 24")
 
-console.log(moment('MMMM Do, YYYY'));
+  // $(times[0]).siblings("textarea").val();
+  for(i=9;i<times.length+9;i++){
+    if(i<actualHour){
 
-console.log(moment().hour());
+      $('#individualForms' + index).addClass("past")
+      
+      //code for recolor
+      //console.log(times[index]+" is less than actual hour")
+   } else if (i>actualHour){
+    //code for recolor
+    $('#individualForms' + index).addClass("future")
+    //console.log(times[index]+" is greater than actual hour")
+   } else {
+     //code for 
+    // console.log(times[index]+" is equal to actual hour")
+    $('#individualForms' + index).addClass("present")
+   }
+  // console.log("index is: "+index)
+   index++;
+  }
+  // console.log($('#individualForms' + 0).val());
+
+  //console.log(moment().hours())
+
+ }
+
+colorHours();
+//console.log(moment('MMMM Do, YYYY'));
+
+//console.log(moment().hour());
 
 
 
