8c216ca01c41b355d08acfab1e60e6737c5d339b
saarahah <sarahlferguson07@gmail.com>
2020-06-19 22:53:17 -0400

    working on local storage

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
 var container = $(".container");
 var times = ["9", "10", "11", "12", "1", "2", "3", "4", "5"];
 // var words = localStorage.getItem("fields");
-var storedFields = JSON.parse(localStorage.getItem("fields"));
+// var storedFields = JSON.parse(localStorage.getItem("fields"));
+var storedFields=[];
 ///makes rows///////////////////////////////////////////
-for (var i = 0; i < times.length; i++) {
+
+  // for(var i = 0; i < times.length; i++){
+     
+             
+  //      }
+
+//console.log(localStorage.getItem("savebutton"+[i]+"fields"));
+ // }
+
+
+for (var i = 0; i < times.length; i=i+1) {
 var hourName = "hour" + i;
 var textInput= "form" +i;
 var savebutton= "savebutton" +i;
 var formsDiv = "individualForms" +i;
 
-    console.log(hourName);
 
-    for()
+if(localStorage.getItem("savebutton"+[i]+"fields")!= null){
+  storedFields[i]=localStorage.getItem("savebutton"+[i]+"fields");
+  console.log("local storage variable name is: " + localStorage.getItem("savebutton"+[i]+"fields") + "i value is: " + i);   
+} else {
+  storedFields[i] = "";
+}
+  //for(var i = 0; i <storedFields.length; i++){
+      // if(localStorage.getItem("savebutton"+[i]+"fields")!= undefined){
+      //       storedFields[i]=localStorage.getItem("savebutton"+[i]+"fields");
+      // }
+
+
+ // }
 
     $( "#individualForms0" ).text(function() {
       return storedFields;
@@ -33,13 +55,18 @@ $('#savebutton' + i).append("SAVE");
 ////////////////////////////////////////////////////////////////////
   $(document).on("click", ".saveBtn", function(e) {  
     alert("hello");
+
+   //var buttonID= $(this).attr('id') + "fields";
+
+
     
 
-localStorage.setItem("fields", JSON.stringify($(this).siblings("textarea").val()));
+localStorage.setItem("storedResponses", JSON.stringify($(this).siblings("textarea").val()));
+//console.log(JSON.parse(localStorage.getItem("savebutton0fields")));
 // $('fields').textContent = words;
 
-   console.log($(this).siblings("textarea").val());
-   console.log(localStorage.fields);
+   //console.log($(this).siblings("textarea").val());
+   //console.log("local store item 0 is: " + localStorage.getItem("savebutton0fields"));
 //var storedFields = JSON.parse(localStorage.getItem("fields"));
 //$(this).Text(storedFields);
    
